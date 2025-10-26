250dd8ec20170224915f5da0a071c912597a6335
Alexander A. Klimov <a@kli.mov>
2025-10-26 14:36:49 +0100

    admin: require Authorization header

--- src/bin/admin.rs
+++ src/bin/admin.rs
@@ -1,9 +1,19 @@
-use cgi::{Request, Response, empty_response, handle};
+use cgi::{Request, Response, empty_response, handle, http::header, text_response};
 
 fn main() {
     handle(handler);
 }
 
-fn handler(_: Request) -> Response {
+fn handler(req: Request) -> Response {
+    if !req.headers().contains_key(header::AUTHORIZATION) {
+        return text_response(
+            511,
+            format!(
+                "\"{}\" header missing.\r\nThe webserver seems not to restrict access to this admin interface.\r\nRefusing operation!\r\n",
+                header::AUTHORIZATION
+            ),
+        );
+    }
+
     empty_response(501)
 }
